600d2dbb268c6b03d4c4b47f74a084d7c2a05002
Salvatore Castellitti <s.castellitti@gmail.com>
2022-06-16 20:09:22 +0200

    Add WS

---
+++ src/handlePoint/handlePoints.js
@@ -0,0 +1,27 @@
+let text;       // variable for the text div you'll create
+let socket = new WebSocket("ws://localhost:8081");
+ 
+function setup() {
+    // The socket connection needs two event listeners:
+    socket.onopen = openSocket;
+    socket.onmessage = showData;
+   
+    // make a new div and position it at 10, 10:
+    text = createDiv("Sensor reading:");
+    text.position(10,10);
+  }
+   
+  function draw() {
+  }
+
+  function openSocket() {
+    text.html("Socket open");
+    socket.send("Hello server");
+  }
+   
+  function showData(result) {
+    // result is a JSON string. Parse it:
+    let input = JSON.parse(result.data);
+    // when the server returns, show the result in the div:
+    text.html("Sensor reading:" + input);
+  }
--- src/serialPortArduino/serialPort.js
+++ src/serialPortArduino/serialPort.js
@@ -25,6 +25,11 @@ function showPortOpen() {
    
   function readSerialData(data) {
     console.log(data);
+    // if there are webSocket connections, send the serial data
+    // to all of them:
+    if (connections.length > 0) {
+      broadcast(data);
+    }
   }
    
   function showPortClose() {
@@ -34,3 +39,30 @@ function showPortOpen() {
   function showError(error) {
     console.log('Serial port error: ' + error);
   }
+
+wss.on('connection', handleConnection);
+ 
+function handleConnection(client) {
+  console.log("New Connection"); // you have a new client
+  connections.push(client); // add this client to the connections array
+ 
+  client.on('message', sendToSerial); // when a client sends a message,
+ 
+  client.on('close', function() { // when a client closes its connection
+    console.log("connection closed"); // print it out
+    let position = connections.indexOf(client); // get the client's position in the array
+    connections.splice(position, 1); // and delete it from the array
+  });
+}
+
+function sendToSerial(data) {
+  console.log("sending to serial: " + data);
+  myPort.write(data);
+}
+
+// This function broadcasts messages to all webSocket clients
+function broadcast(data) {
+  for (myConnection in connections) {   // iterate over the array of connections
+    connections[myConnection].send(data); // send the data to each connection
+  }
+}
