58aa9366cbd8eab58fa28065767209b791656bb4
Salvatore Castellitti <s.castellitti@gmail.com>
2022-06-17 23:36:58 +0200

    added Websocket and change on new points

--- src/handlePoint/handlePoints.js
+++ src/handlePoint/handlePoints.js
@@ -1,6 +1,24 @@
 let text;       // variable for the text div you'll create
+const WebSocket = require('ws');
 let socket = new WebSocket("ws://localhost:8081");
  
+let dataPars = "weasdsadas"
+socket.onmessage = showData;
+function showData(result) {
+  // result is a JSON string. Parse it:
+  let input = JSON.stringify(result.data);
+  dataPars=input
+  console.log(input)
+  
+}
+
+function myFunc(){
+  document.body.innerHTML = "<h1>" + "sdadasds" + "</h1>"
+}
+
+document.body.innerHTML = "<h1>" + "sdadasds" + "</h1>"
+
+/*
 function setup() {
     // The socket connection needs two event listeners:
     socket.onopen = openSocket;
@@ -22,6 +40,9 @@ function setup() {
   function showData(result) {
     // result is a JSON string. Parse it:
     let input = JSON.parse(result.data);
+    console.log(result)
+
+    console.log(input)
     // when the server returns, show the result in the div:
-    text.html("Sensor reading:" + input);
-  }
+    //text.html("Sensor reading:" + input);
+  }*/
