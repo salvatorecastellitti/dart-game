bf668993f539cd415df24d817903cd0584334776
Salvatore Castellitti <s.castellitti@gmail.com>
2022-06-18 21:41:26 +0200

    Added svg for dart left

--- src/index.js
+++ src/index.js
@@ -4,26 +4,36 @@ const path = require('path')
 var punteggio1 = 501
 var punteggio2 = 501
 var turnoPlayer = true
-var contaFrecce = 0
+var contaFrecce1 = 0
+var contaFrecce2 = 0
+var remainingDart1 = 0
+var remainingDart2 = 0
 
 
-//definiamo la connessione alla websocket
+
+//definiamo la connessione alla websocketx  
 const WebSocket = require('ws');
 let socket = new WebSocket("ws://localhost:8081");
 
 //definiamo le variabili id che saranno presenti nell'html
 var score1 = document.getElementById('score1')
 var score2 = document.getElementById('score2')
-var dartLeft1 = document.getElementById('dartLeft1')
-var dartLeft2 = document.getElementById('dartLeft2')
+//var dartLeft1 = document.getElementById('dartLeft1')
+//var dartLeft2 = document.getElementById('dartLeft2')
 
 score1.innerHTML = punteggio1
 score2.innerHTML = punteggio2
-dartLeft1.innerHTML = 3
-dartLeft2.innerHTML = 3
+//dartLeft1.innerHTML = 3
+//dartLeft2.innerHTML = 3
 //socket.onmessage, ogni qual volta riceve un messagio esegue una funzione, in questo caso show data
 socket.onmessage = showData;
-
+//dartLeft.innerHTML += `<img src="./assets/imgs/darts-svgrepo-com.svg"/>` + `<img src="./assets/imgs/darts-svgrepo-com.svg"/>`
+for(var i=3;i>contaFrecce1;i--){
+    dartLeft1.innerHTML += `<img src="./assets/imgs/darts-svgrepo-com.svg"/>`
+}
+for(var i=3;i>contaFrecce2;i--){
+    dartLeft2.innerHTML += `<img src="./assets/imgs/darts-svgrepo-com.svg"/>`
+}
 //gestisce il messaggio
 function showData(result) {
     // result is a JSON string. Parse it:
@@ -39,32 +49,44 @@ function showData(result) {
 function calcoloPunteggio(placeHolderFreccia) {
 
     let puntoFreccia = points[placeHolderFreccia]
-    contaFrecce++
     if (turnoPlayer == true) {
         if (puntoFreccia > punteggio1) {
             turnoPlayer = !turnoPlayer
-            contaFrecce = 0
+            contaFrecce1 = 0
         } else {
+            contaFrecce1++
             punteggio1 = punteggio1 - puntoFreccia
             score1.innerHTML = punteggio1
-            dartLeft1.innerHTML = 3-contaFrecce
+            dartLeft1.innerHTML = ``
+            for(var i=3;i>contaFrecce1;i--){
+                
+                dartLeft1.innerHTML += `<img src="./assets/imgs/darts-svgrepo-com.svg"/>`
+            }
+            
         }
 
     } else {
         if (puntoFreccia > punteggio2) {
             turnoPlayer = !turnoPlayer
-            contaFrecce = 0
+            contaFrecce2 = 0
         } else {
+            contaFrecce2++
             punteggio2 = punteggio2 - puntoFreccia
             score2.innerHTML = punteggio2
-            dartLeft2.innerHTML = 3-contaFrecce
+            dartLeft2.innerHTML = ``
+            for(var i=3;i>contaFrecce2;i--){
+                
+                dartLeft2.innerHTML += `<img src="./assets/imgs/darts-svgrepo-com.svg"/>`
+            }
         }
 
     }
-    if (contaFrecce == 3) {
+    if (contaFrecce1 == 3) {
         turnoPlayer = !turnoPlayer
-        contaFrecce = 0
-
+        contaFrecce1 = 0
+    }else if (contaFrecce2 ==3){
+        turnoPlayer = !turnoPlayer
+        contaFrecce2 = 0
     }
 }
 
